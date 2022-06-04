75e0df511a498af82c769702ca6336d46ece2c52
Wadimx199789 <martinkewichwadim@gmail.com>
2022-06-05 01:28:27 +0300

    feat: add animation sections for project

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,57 +1,74 @@
-window.onload = function () {
-    const arrLinks = document.querySelectorAll('.menu__link');
-    const advertisements = document.querySelectorAll(".advertisement__word");
-    const flask = document.querySelector(".flasks__flask");
-    const flaskContent = document.querySelector(".flasks__content");
+const categoryInner = document.querySelector(".category__inner")
+const words = document.querySelectorAll(".advertisement__word");
+const blocks = document.querySelectorAll(".category__inner");
+const flasks = document.querySelectorAll(".flasks__flask");
+const flasksContent = document.querySelectorAll(".flasks__flask-content")
 
-    flask.addEventListener("click",function(){
-        flaskContent.styles.left=0+"px";
-    })
+flasks.forEach((flask) => {
+    flask.addEventListener("click", (e) => {
+        if (!flask.classList.contains("active")) {
+            flask.classList.add("active");
+            flasks.forEach((flask) => {
+                if (!flask.classList.contains("active")) {
+                    flask.classList.add("hide");
+                }
+            })
+        }
+        else {
+            flask.classList.remove("active");
+            flasks.forEach((flask) => {
+                if (flask.classList.contains("hide")) {
+                    flask.classList.remove("hide");
+                }
+            })
+        }
+        flasksContent.forEach((flasksContent) => {
+            if (flasksContent.getAttribute("data-flask") === e.target.parentNode.getAttribute("data-flask")) {
+                switch (e.target.parentNode.getAttribute("data-flask")) {
+                    case "light": if (!flasksContent.classList.contains("active")) {
+                        flasksContent.classList.add("active");
+                    }
+                    else {
+                        flasksContent.classList.remove("active");
+                    };
+                        break;
+                    case "business": if (!flasksContent.classList.contains("active")) {
+                        flasksContent.classList.add("active");
+                    }
+                    else {
+                        flasksContent.classList.remove("active");
+                    };
+                        break;
+                    case "creative": if (!flasksContent.classList.contains("active")) {
+                        flasksContent.classList.add("active");
+                    }
+                    else {
+                        flasksContent.classList.remove("active");
+                    };
+                        break;
 
-    advertisements.forEach((link)=>{
-        link.addEventListener("click",function(e){
-            e.target.classList.toggle("active");
+                }
+
+
+            }
         })
-    })
-
-
-    ScrollToBottomBtn.addEventListener('click', function () {
-
-        scrollMenu(this.getAttribute('href'));
-    })
-
-    for (let i = 0; i < arrLinks.length; i++) {
-
-        arrLinks[i].addEventListener('click', function () {
-
-            scrollMenu(this.getAttribute('href'));
+    });
+})
+words.forEach((word) => {
+    word.addEventListener("click", (e) => {
+        if (!categoryInner.classList.contains("hide")) {
+            categoryInner.classList.add("hide")
+        }
+        blocks.forEach((block) => {
+            if (block.getAttribute("data-name") === e.target.getAttribute("data-name")) {
+                blocks.forEach((block) => {
+                    if (block.classList.contains("active")) {
+                        block.classList.remove("active")
+                    }
+                })
+                block.classList.add("active")
+            }
         })
 
-    }
-};
-
-
-// scroll function
-function scrollMenu(blockId) {
-    let temp, start, from, to;
-    //scroll animation
-    cancelAnimationFrame(temp);
-    start = performance.now();
-    from = window.pageYOffset || document.documentElement.scrollTop;
-    to = document.querySelector(blockId).getBoundingClientRect().top - 33;
-    if (blockId === "#lab") {
-        duration = 1000 * Math.abs(to) / 1000;
-    } else
-        duration = 1000 * Math.abs(to) / 5000;
-
-    // scroll
-    requestAnimationFrame(function step(timestamp, e) {
-        var progress = (timestamp - start) / duration;
-        1 <= progress && (progress = 1);
-        window.scrollTo(0, from + to * progress | 0);
-        (1 > progress) ? temp = requestAnimationFrame(step) : (document.location.hash = blockId);
-        document.addEventListener("wheel", function () {
-            cancelAnimationFrame(temp);
-        })
     })
-}
+});
