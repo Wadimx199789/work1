62c1cd90fdcd9c8c49636499f8b561415c9bbc3e
Wadimx199789 <martinkewichwadim@gmail.com>
2022-06-06 18:12:07 +0300

    feat: add update page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,48 @@
+window.onload = function () {
+    const arrLinks = document.querySelectorAll('.menu__list-link');
+    const ScrollToBottomBtn = document.querySelector('.pulse-btn');
+    const burgerButton = document.querySelector('.burger');
+    const menuLinks = document.querySelectorAll(".menu__list-link")
+    const menu = document.querySelector('.menu');
+
+
+    burgerButton.addEventListener("click",function(){
+        burgerButton.classList.toggle("active");
+        if(burgerButton.classList.contains("active")){
+            menu.classList.add("active");
+
+        }
+        else{
+            menu.classList.remove("active");
+        }
+        
+    });
+    menuLinks.forEach((menuLink)=>{
+        menuLink.addEventListener("click",function(e){
+            menu.classList.remove("active");
+            burgerButton.classList.remove("active");
+        })
+    })
+    ScrollToBottomBtn.addEventListener('click', function () {
+
+        scrollMenu(this.getAttribute('href'));
+    })
+
+    for (let i = 0; i < arrLinks.length; i++) {
+
+        arrLinks[i].addEventListener('click', function () {
+
+            scrollMenu(this.getAttribute('href'));
+        })
+
+    }
+};
+
 const categoryInner = document.querySelector(".category__inner")
 const words = document.querySelectorAll(".advertisement__word");
 const blocks = document.querySelectorAll(".category__inner");
 const flasks = document.querySelectorAll(".flasks__flask");
-const flasksContent = document.querySelectorAll(".flasks__flask-content")
+const flasksContent = document.querySelectorAll(".flasks__flask-content");
 
 flasks.forEach((flask) => {
     flask.addEventListener("click", (e) => {
@@ -14,7 +54,7 @@ flasks.forEach((flask) => {
                 }
             })
         }
-        else {
+        else if(flask.classList.contains("active")){
             flask.classList.remove("active");
             flasks.forEach((flask) => {
                 if (flask.classList.contains("hide")) {
@@ -72,3 +112,29 @@ words.forEach((word) => {
 
     })
 });
+
+
+// scroll function
+function scrollMenu(blockId) {
+    let temp, start, from, to;
+    //scroll animation
+    cancelAnimationFrame(temp);
+    start = performance.now();
+    from = window.pageYOffset || document.documentElement.scrollTop;
+    to = document.querySelector(blockId).getBoundingClientRect().top;
+    // if (blockId === "#lab") {
+    //     duration = 1000 * Math.abs(to) / 1000;
+    // } else
+        duration = 1000 * Math.abs(to) / 4000;
+
+    // scroll
+    requestAnimationFrame(function step(timestamp, e) {
+        var progress = (timestamp - start) / duration;
+        1 <= progress && (progress = 1);
+        window.scrollTo(0, from + to * progress | 0);
+        (1 > progress) ? temp = requestAnimationFrame(step) : (document.location.hash = blockId);
+        document.addEventListener("wheel", function () {
+            cancelAnimationFrame(temp);
+        })
+    })
+}
